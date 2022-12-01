38aa4862d87168edcd1b1854a331d8597ee48d5a
onejaelee <ol2217@columbia.edu>
2022-11-30 19:16:43 -1000

    implemented tweet search

--- tweetextract.py
+++ tweetextract.py
@@ -6,15 +6,28 @@ Created on Wed Nov 30 15:12:25 2022
 """
 
 
+
+
 import tweepy
 
 #Initialize keys from your Twitter Dev Account
-consumerKey = "consumerkey"
-consumerSecret = "consumersecret"
+APIKey = "consumerkey"
+APISecret = "consumersecret"
 accessToken = "accesstoken"
-accessTokenSecret = "accesstokenseceret"
+accessTokenSecret = "accesstokensecret"
 
-
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth = tweepy.OAuthHandler(APIKey, APISecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
+
+#Keyword or hashtag to search
+keyword = "mango"
+#quantity of tweets to input
+countTweet = 5
+
+#Fetch tweets, you need elevated access for this
+tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(countTweet)
+
+#iterate through tweets and print
+for tweet in tweets:
+    print(tweet)
