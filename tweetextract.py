5b727995eaa5a2a9f815294d2f1ef8c167ec67ee
onejaelee <ol2217@columbia.edu>
2022-11-30 21:28:12 -1000

    implemented preprocessing

--- tweetextract.py
+++ tweetextract.py
@@ -5,16 +5,29 @@ Created on Wed Nov 30 15:12:25 2022
 @author: One
 """
 
-
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
 
 
 import tweepy
 
+
+def preprocessTw(tweet):
+    for word in tweet.split(' '):
+        if word[0] == '@' and len(word) > 1:
+            word = "@user"
+        elif word.startswith('http'):
+            word = "http"
+        tweet.append(word)
+    tweetproc = " ".join(tweet)
+    return tweetproc
 #Initialize keys from your Twitter Dev Account
 APIKey = "consumerkey"
 APISecret = "consumersecret"
 accessToken = "accesstoken"
 accessTokenSecret = "accesstokensecret"
+
+
 
 auth = tweepy.OAuthHandler(APIKey, APISecret)
 auth.set_access_token(accessToken, accessTokenSecret)
@@ -23,10 +36,10 @@ api = tweepy.API(auth)
 #Keyword or hashtag to search
 keyword = "mango"
 #quantity of tweets to input
-countTweet = 5
+countTweet = 2
 
 #Fetch tweets, you need elevated access for this
-tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(countTweet)
+tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang = "en").items(countTweet)
 
 #iterate through tweets and print
 for tweet in tweets:
